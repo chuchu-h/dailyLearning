572be7b5bd24a3968c997e15059073c3640cc0d5
chuxingyu.cxy <chuxingyu.cxy@digital-engine.com>
2025-03-03 23:47:21 +0800

    feat: 手写call\bind\apply

--- call-bind-apply.ts
+++ call-bind-apply.ts
@@ -28,3 +28,54 @@ fun.apply(obj, ['红色', 18])
  */
 let fun2 = fun.bind(obj, '红色', 18)
 fun2()
+
+
+// 原生JavaScript实现bind方法
+Function.prototype.bind = function (context) {
+  let self = this;
+  return function () {
+    return self.apply(context, arguments)
+  }
+}
+// 原生JavaScript实现call方法
+Function.prototype.myCall = function (context, ...args) {
+  if (typeof this !== 'function') {
+    throw Error('Function.prototype.call is not a function')
+  }
+  context = context || globalThis;
+  const fn = Symbol('fn')
+  context[fn] = this
+  const result = context[fn](...args)
+  delete context[fn]
+  return result
+}
+
+// 原生JavaScript实现apply方法
+Function.prototype.apply = function (context, args) {
+  if (typeof this !== 'function') {
+    throw Error('Function.prototype.call is not a function')
+  }
+  if (args && !Array.isArray(args)) {
+    throw Error('args must be array')
+  }
+  context = context || globalThis
+  const fn = Symbol('fn')
+  context[fn] = this
+  const result = context[fn](...args)
+  delete context[fn]
+  return result
+  //  return this.call(context, ...args)
+}
+// 原生JavaScript实现bind方法
+Function.prototype.myBind = function (context, ...args) {
+  if (typeof this !== 'function') {
+    throw Error('Function.prototype.call is not a function')
+  }
+  const _this = this;
+  return function (...args2) {
+    if (this instanceof function) {
+      return new _this(...args, ...args2)
+    }
+    return _this.call(context, ...args, ...args2)
+  }
+}
